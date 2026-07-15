02af4b86e15d2cbfeb8742a8568725a211814b02
Gilberto Junior <giljrsantos@gmail.com>
2026-07-15 14:36:40 -0300

    first commit

---
+++ src/main/java/com/apprh/AppRh_Hexagonal/AppRhHexagonal.java
@@ -0,0 +1,13 @@
+package com.apprh.AppRh_Hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRhHexagonal {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRhHexagonal.class, args);
+	}
+
+}
---
+++ src/test/java/com/apprh/AppRh_Hexagonal/AppRhHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apprh.AppRh_Hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRhHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
