ace4a3bb0418b2a4ae91b6ff609ae43320f00191
Gilberto Junior <giljrsantos@gmail.com>
2026-07-16 19:05:41 -0300

    Controller Heath para ver a saude da aplicação

---
+++ src/main/java/com/apprh/AppRh_Hexagonal/adapter/controller/HeathController.java
@@ -0,0 +1,21 @@
+package com.apprh.AppRh_Hexagonal.adapter.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+@RequiredArgsConstructor
+public class HeathController {
+
+    @GetMapping("health")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck() {
+        return "A Aplicação AppRh Arquitetura Hexagonal está rodando!";
+    }
+}
