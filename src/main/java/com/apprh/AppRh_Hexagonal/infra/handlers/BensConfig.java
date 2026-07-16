776d85d025156dbe284b97e82e4b383843118304
Gilberto Junior <giljrsantos@gmail.com>
2026-07-16 20:44:20 -0300

    add BensConfig class for application configuration and bean definitions

---
+++ src/main/java/com/apprh/AppRh_Hexagonal/infra/handlers/BensConfig.java
@@ -0,0 +1,22 @@
+package com.apprh.AppRh_Hexagonal.infra.handlers;
+
+import com.apprh.AppRh_Hexagonal.core.ports.VagaRepositoryPort;
+import com.apprh.AppRh_Hexagonal.core.ports.VagaServicePort;
+import com.apprh.AppRh_Hexagonal.core.services.VagaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BensConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public VagaServicePort vagaServicePort(VagaRepositoryPort vagaRepositoryPort){
+        return new VagaService(vagaRepositoryPort);
+    }
+}
