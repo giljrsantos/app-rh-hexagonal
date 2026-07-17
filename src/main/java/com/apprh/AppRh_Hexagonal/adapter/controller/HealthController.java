1004b091e50ffd386ead05f97e1ffe2a3bfb56f9
Gilberto Junior <giljrsantos@gmail.com>
2026-07-17 19:01:49 -0300

    Correção do nome da função

--- src/main/java/com/apprh/AppRh_Hexagonal/adapter/controller/HealthController.java
+++ src/main/java/com/apprh/AppRh_Hexagonal/adapter/controller/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/")
 @RequiredArgsConstructor
-public class HeathController {
+public class HealthController {
 
     @GetMapping("health")
     @ResponseStatus(HttpStatus.OK)
