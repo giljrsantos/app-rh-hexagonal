12909a266193f6b227baa2a97e275e365a3aeb48
Gilberto Junior <giljrsantos@gmail.com>
2026-07-19 15:57:30 -0300

    Implementação dos valores padrão para ser salvo quando for feito o cadastro da vaga. A dataCadastro e dataAtualizacao. Sendo que a dataCadastro o updatable igual a false para que esse campo nunca possa ser alterado.

--- src/main/java/com/apprh/AppRh_Hexagonal/adapter/entities/VagaEntity.java
+++ src/main/java/com/apprh/AppRh_Hexagonal/adapter/entities/VagaEntity.java
@@ -1,12 +1,10 @@
 package com.apprh.AppRh_Hexagonal.adapter.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -24,6 +22,13 @@ public class VagaEntity {
     private String descricao;
     private BigDecimal salario;
     private Boolean status; // true = aberta; false = fechada
+
+    @CreationTimestamp
+    @Column(name = "dataCadastro", updatable = false)
     private LocalDate dataCadastro;
+
+    @CreationTimestamp
+    @Column(name = "dataAtualizacao")
+    private LocalDate dataAtualizacao;
 
 }
