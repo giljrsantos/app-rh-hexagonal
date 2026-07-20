24b0fe17fa869b78a636352be93e7801c7ec7538
Gilberto Junior <giljrsantos@gmail.com>
2026-07-20 07:59:38 -0300

    Atualização da anotação da dataAtualizacao para usar @UpdateTimestamp

--- src/main/java/com/apprh/AppRh_Hexagonal/adapter/entities/VagaEntity.java
+++ src/main/java/com/apprh/AppRh_Hexagonal/adapter/entities/VagaEntity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -27,7 +28,7 @@ public class VagaEntity {
     @Column(name = "dataCadastro", updatable = false)
     private LocalDate dataCadastro;
 
-    @CreationTimestamp
+    @UpdateTimestamp
     @Column(name = "dataAtualizacao")
     private LocalDate dataAtualizacao;
 
