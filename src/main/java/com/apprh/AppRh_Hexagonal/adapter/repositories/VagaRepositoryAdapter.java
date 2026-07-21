31ab601b191fed02978c1395c860f336f583616e
Gilberto Junior <giljrsantos@gmail.com>
2026-07-21 18:24:45 -0300

    Implementação do método update na VagaRepositoryAdapter para atualizar uma vaga com tratamento de exceção

--- src/main/java/com/apprh/AppRh_Hexagonal/adapter/repositories/VagaRepositoryAdapter.java
+++ src/main/java/com/apprh/AppRh_Hexagonal/adapter/repositories/VagaRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.apprh.AppRh_Hexagonal.adapter.repositories;
 
 import com.apprh.AppRh_Hexagonal.adapter.entities.VagaEntity;
 import com.apprh.AppRh_Hexagonal.core.domain.Vaga;
+import com.apprh.AppRh_Hexagonal.core.exception.BusinessNotFoundException;
 import com.apprh.AppRh_Hexagonal.core.ports.VagaRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -35,6 +36,24 @@ public class VagaRepositoryAdapter implements VagaRepositoryPort {
     @Override
     public Optional<Vaga> getById(Long id){
         return vagaRepository.findById(id).map(vagaEntity -> modelMapper.map(vagaEntity, Vaga.class));
+    }
+
+    public Vaga update(Vaga vaga){
+        VagaEntity entity = modelMapper.map(vaga, VagaEntity.class);
+
+        //Buscar a entidade existente para manter a vaga atual
+        VagaEntity vagaExistente = vagaRepository.findById(vaga.getId())
+                .orElseThrow(() -> new BusinessNotFoundException("Vaga não encontrada com esse ID: " + vaga.getId()));
+
+
+        if(!vagaExistente.getId().equals(vaga.getId())){
+            vagaRepository.findById(vaga.getId()).ifPresent(v -> {
+                throw new BusinessNotFoundException("Vaga não encontrada com esse ID: " + vaga.getId());
+            });
+        }
+
+        VagaEntity update = vagaRepository.save(entity);
+        return modelMapper.map(update, Vaga.class);
     }
 
 }
