62d207d09f1572c27ff60240b7a087b789c17cb9
Gilberto Junior <giljrsantos@gmail.com>
2026-07-23 19:46:52 -0300

    Adição do atributo titulo e alteração do tipo de dataAtualizacao para LocalDateTime na classe Vaga

--- src/main/java/com/apprh/AppRh_Hexagonal/core/domain/Vaga.java
+++ src/main/java/com/apprh/AppRh_Hexagonal/core/domain/Vaga.java
@@ -2,26 +2,37 @@ package com.apprh.AppRh_Hexagonal.core.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Vaga {
 
     private Long id;
+    private String titulo;
     private String descricao;
     private BigDecimal salario;
     private Boolean status; // true = aberta; false = fechada
     private LocalDate dataCadastro;
-    private LocalDate dataAtualizacao;
+    private LocalDateTime dataAtualizacao;
 
     public Vaga() {
     }
 
-    public Vaga(Long id, String descricao, BigDecimal salario, Boolean status, LocalDate dataCadastro, LocalDate dataAtualizacao) {
+    public Vaga(Long id, String titulo, String descricao, BigDecimal salario, Boolean status, LocalDate dataCadastro, LocalDateTime dataAtualizacao) {
         this.id = id;
+        this.titulo = titulo;
         this.descricao = descricao;
         this.salario = salario;
         this.status = status;
         this.dataCadastro = dataCadastro;
         this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public Long getId() {
@@ -65,11 +76,11 @@ public class Vaga {
         this.dataCadastro = dataCadastro;
     }
 
-    public LocalDate getDataAtualizacao() {
+    public LocalDateTime getDataAtualizacao() {
         return dataAtualizacao;
     }
 
-    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
         this.dataAtualizacao = dataAtualizacao;
     }
 }
